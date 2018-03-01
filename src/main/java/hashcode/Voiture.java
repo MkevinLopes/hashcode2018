542aab825c9a91204cdb48a94d4846e2b86821bd
Jason BOURLARD <jason.bourlard@soprasteria.com>
2018-03-01 20:57:41 +0100

    Algo de jason

--- src/main/java/hashcode/Voiture.java
+++ src/main/java/hashcode/Voiture.java
@@ -2,6 +2,10 @@ package hashcode;
 
 public class Voiture {
 	private Coordonnees position;
+	
+	public Trajet ride;
+	
+	public boolean isRiding;
 
 	public Coordonnees getPosition() {
 		return position;
