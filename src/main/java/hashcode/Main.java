24725ea3494623439dbe88502513e778f2e952b1
ami <arthur.milhade@soprasteria.com>
2018-03-01 20:50:01 +0100

    Correction lancement

--- src/main/java/hashcode/Main.java
+++ src/main/java/hashcode/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException
 	{
-		Grille grille = lireFichier("‪toto");
+		Grille grille = lireFichier("a_example.in");
 		
 		System.out.println("coucou");
 	}
